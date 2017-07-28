8505105d8c2e69ea5ccfd47c17f9597deee4f4bf
Shauryasimplilearn <ashishmurgai30@gmail.com>
2017-07-29 00:49:50 +0530

    Logger applied

--- src/main/java/convertor/NumberToWordsConvertor.java
+++ src/main/java/convertor/NumberToWordsConvertor.java
@@ -47,29 +47,29 @@ public class NumberToWordsConvertor
 
     private String convertLessThanOneThousand(int number) {
         String current;
-        System.out.println("Inside convertLessThanOneThousand number"+" " +number);
-        System.out.println("Before Calculation for "+" "+ (number % 100));
+        Logger.getLogger("Inside convertLessThanOneThousand number"+" " +number);
+        Logger.getLogger("Before Calculation for "+" "+ (number % 100));
         
         if (number % 100 < 20){
-        	System.out.println("Inside < 20 if condition");
+        	Logger.getLogger("Inside < 20 if condition");
             current = numNames[number % 100];
-            System.out.println("Value for Current variable"+" "+current);
+            Logger.getLogger("Value for Current variable"+" "+current);
             number /= 100;
-            System.out.println("Value for number/100"+" "+(number));
+            Logger.getLogger("Value for number/100"+" "+(number));
         }
         else {
-        	System.out.println("Inside else condition");
+        	Logger.getLogger("Inside else condition");
             current = numNames[number % 10];
-            System.out.println("Value for Current variable"+" "+current);
+            Logger.getLogger("Value for Current variable"+" "+current);
             number /= 10;
-            System.out.println("Value for number/10"+" "+(number));
+            Logger.getLogger("Value for number/10"+" "+(number));
             current = tensNames[number % 10] + current;
-            System.out.println("After tensNames"+" "+(number % 10));
+            Logger.getLogger("After tensNames"+" "+(number % 10));
             number /= 10;
-            System.out.println("After number /= 10"+" "+(number));
+            Logger.getLogger("After number /= 10"+" "+(number));
         }
-        System.out.println("Final number variable value before return"+" "+number);
-        System.out.println("Final current variable value before return"+" "+current);
+        Logger.getLogger("Final number variable value before return"+" "+number);
+        Logger.getLogger("Final current variable value before return"+" "+current);
         if (number == 0) return current;
         return numNames[number] + " hundred" + " "+current;
     }
@@ -79,7 +79,7 @@ public class NumberToWordsConvertor
      * @return String
      */
     public String convert(int number) {
-    	System.out.println("Inside Convert() Function"+" "+number);
+    	Logger.getLogger("Inside Convert() Function"+" "+number);
 
         if (number == 0) { return "zero"; }
         
@@ -95,28 +95,28 @@ public class NumberToWordsConvertor
         
         do {
             int n = number % 1000;
-            System.out.println("Int number is"+" "+n);
+            Logger.getLogger("Int number is"+" "+n);
             if (n != 0){
-            	System.out.println("Inside n!0 number"+" "+n);
+            	Logger.getLogger("Inside n!0 number"+" "+n);
                 String s = convertLessThanOneThousand(n);
-                System.out.println("String S variable value is"+" "+s);
-                System.out.println("Value of Current variable value is"+" "+current);
-                System.out.println("Value of place variable value is"+" "+place);
+                Logger.getLogger("String S variable value is"+" "+s);
+                Logger.getLogger("Value of Current variable value is"+" "+current);
+                Logger.getLogger("Value of place variable value is"+" "+place);
                 current = s + current;
             }
           number /= 1000;
         } while (number > 0);
-        System.out.println("Value of prefix variable value is"+"*****"+" "+prefix);
-        System.out.println("Value of prefix current value is"+"*******"+" "+current);
+        Logger.getLogger("Value of prefix variable value is"+"*****"+" "+prefix);
+        Logger.getLogger("Value of prefix current value is"+"*******"+" "+current);
         return (prefix + current).trim();
     }
     
     public static void main(String[] args) {
     	NumberToWordsConvertor obj = new NumberToWordsConvertor();
     	
-    		System.out.println("ARGS[0] is"+" "+args[0]);
+    		Logger.getLogger("ARGS[0] is"+" "+args[0]);
     	if(obj.isValid(args[0]))
-    		System.out.println("####   " + obj.convert(Integer.parseInt(args[0])));
+    		Logger.getLogger("####   " + obj.convert(Integer.parseInt(args[0])));
     	else 
     		return;
     }
@@ -126,12 +126,12 @@ public class NumberToWordsConvertor
       	
     	if(!(val.trim()).equals("") && !isAlphanumeric(val.trim()) && Integer.parseInt(val)!=0 && val.trim().length()<=3)
         {
-    		System.out.println("Inside True Condition");
+    		Logger.getLogger("Inside True Condition");
        		return true;
         }
         else
         {   
-        	System.out.println("Inside False Condition");
+        	Logger.getLogger("Inside False Condition");
         	return false;
         }
 
@@ -141,9 +141,9 @@ public class NumberToWordsConvertor
     	int counter=0;
         for (int i=0; i<str.length(); i++) {
             char c = str.charAt(i);
-            System.out.println("Character at"+c);
-            System.out.println("Is Digital"+Character.isDigit(c));
-            System.out.println("Is Character"+Character.isLetter(c));
+            Logger.getLogger("Character at"+c);
+            Logger.getLogger("Is Digital"+Character.isDigit(c));
+            Logger.getLogger("Is Character"+Character.isLetter(c));
             if (c=='-'|| Character.isDigit(c) && !Character.isLetter(c))
             {
             	counter++;
@@ -151,8 +151,8 @@ public class NumberToWordsConvertor
             }
                 
         }
-        System.out.println("Value for Counter is"+counter);
-        System.out.println("Value for str is"+str.length());
+        Logger.getLogger("Value for Counter is"+counter);
+        Logger.getLogger("Value for str is"+str.length());
         if(counter==str.length())
         	return false;
         else 
